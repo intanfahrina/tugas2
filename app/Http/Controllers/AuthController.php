bdb21e1e151211f3bca5d00e37383bd7be8f4ce4
intanfahrina <intanfahrina07@gmail.com>
2024-02-07 23:52:44 +0700

    Tugas 2

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\View\View;
+use Session;
+
+
+class AuthController extends Controller
+{
+
+    //
+    public function index()
+    {
+        if (Auth::check())
+        {
+            return view('students.index');
+        }
+        else
+        {
+            return view("auth.login");
+        }
+    }
+
+    public function postlogin (Request $request)
+    {
+
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+    $data = [
+        'email' => $request->email,
+        'password' => $request->password
+    ];
+
+    if(Auth::attempt($data)){
+        return redirect('student');
+    } else {
+        return back();
+    }
+    }
+
+    public function regis()
+    {
+        return view('auth.register');
+    }
+
+    public function store(Request $request)
+    {
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'remember_token' => Str::random(60),
+        ]);
+
+
+        return redirect ('login');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    }
+
+
+}
+
+
+
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/login', [AuthController::class, 'index'])->name('login');
+Route:: post('/postlogin', [AuthController::class, 'postlogin'])->name('postlogin');
+
+Route::get('/register', [AuthController::class, 'regis'])->name('register');
+Route::post('/store', [AuthController::class, 'store'])->name('authregister');
+
+Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+
+Route::get('/', function () {
+    return redirect(route('login'));
+});
+
+
+Route::resource('/student', StudentController::class)->middleware('auth');
+
